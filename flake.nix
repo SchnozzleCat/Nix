0b69f0b6b961855d5e71c354c4ee338cd8985365
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-13 22:56:41 +0100

    fix: deprecated code

--- flake.nix
+++ flake.nix
@@ -28,11 +28,7 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
-    # Supported systems for your flake packages, shell, etc.
     system = "x86_64-linux";
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
-    forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
