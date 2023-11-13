b4b7342734eba8d170e8a6d4a85840dd98f93158
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-13 22:56:11 +0100

    fix: system version

--- flake.nix
+++ flake.nix
@@ -29,13 +29,7 @@
   } @ inputs: let
     inherit (self) outputs;
     # Supported systems for your flake packages, shell, etc.
-    systems = [
-      "aarch64-linux"
-      "i686-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
+    system = "x86_64-linux";
     # This is a function that generates an attribute by calling a function you
     # pass to it, with each system as an argument
     forAllSystems = nixpkgs.lib.genAttrs systems;
