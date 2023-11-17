373a799d6dc5266ae3e3b7589efd8a5dcb9bffff
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-17 04:11:17 +0100

    feat: add foot config; add btop config

--- home/linus.nix
+++ home/linus.nix
@@ -94,6 +94,48 @@ in
       userEmail = "git@schnozzlecat.com";
   };
 
+  programs.foot = {
+    enable = true;
+    settings = {
+      font = "JetBrainsMono Nerd Font:size=10";
+      cursor = {
+        color="1A1826 D9E0EE";
+      };
+      colors = {
+        alpha=0.95;
+        foreground=colors.base05;
+        background=colors.base00;
+        regular0=colors.base02;
+        regular1=colors.base08;
+        regular2=colors.base0B;
+        regular3=colors.base09;
+        regular4=colors.base0D;
+        regular5=colors.base0E;
+        regular6=colors.base0C;
+        regular7=colors.base06;
+        bright0=colors.base02;
+        bright1=colors.base08;
+        bright2=colors.base0B;
+        bright3=colors.base09;
+        bright4=colors.base0D;
+        bright5=colors.base0E;
+        bright6=colors.base0C;
+        bright7=colors.base06;
+      };
+    };
+  };
+
+  programs.btop = {
+    enable = true;
+    settings = {
+      color_theme = "ayu";
+      theme_background = false;
+      truecolor = true;
+      rounded_corners = true;
+      vim_keys = true;
+    };
+  };
+
   programs.fuzzel = {
     enable = true;
     settings = {
