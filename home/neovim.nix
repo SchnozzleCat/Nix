d80d6300810bc3d99a97cb725927151b3188ed70
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-22 20:45:52 +0100

    feat: update nixvim config

--- home/neovim.nix
+++ home/neovim.nix
@@ -8,35 +8,243 @@
 }: {
   programs.nixvim = {
     enable = true;
+    extraPlugins = with pkgs.vimPlugins; [
+      vim-move
+    ];
+    extraConfigVim = ''
+      autocmd BufWritePre * lua vim.lsp.buf.format()
+      autocmd FileType nix setlocal commentstring=#\ %s
+    '';
+    extraConfigLua = ''
+      vim.opt.pumheight = 10
+    '';
     options = {
       number = true;
       undofile = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+      autoindent = true;
+      smartindent = false;
+      cursorline = true;
+      clipboard = "unnamedplus";
     };
     globals = {
       mapleader = " ";
     };
     colorschemes.catppuccin.enable = true;
     keymaps = [
+      # Misc
       {
         mode = "n";
-        key = "<leader>b";
-        action = "<cmd> enew <CR>";
-        options.desc = "New Buffer";
+        key = "<esc>";
+        action = "<cmd> noh <cr>";
+      }
+      {
+        mode = "i";
+        key = "<c-j>";
+        action = "<down>";
+      }
+      {
+        mode = "i";
+        key = "<c-k>";
+        action = "<up>";
+      }
+      {
+        mode = "i";
+        key = "<c-h>";
+        action = "<left>";
+      }
+      {
+        mode = "i";
+        key = "<c-l>";
+        action = "<right>";
+      }
+      # Windows
+      {
+        mode = "n";
+        key = "<c-j>";
+        action = "<c-w>j";
       }
       {
         mode = "n";
+        key = "<c-k>";
+        action = "<c-w>k";
+      }
+      {
+        mode = "n";
+        key = "<c-h>";
+        action = "<c-w>h";
+      }
+      {
+        mode = "n";
+        key = "<c-l>";
+        action = "<c-w>l";
+      }
+      # LSP
+      {
+        mode = "n";
+        key = "gr";
+        action = "<cmd> lua vim.lsp.buf.references() <cr>";
+        options.desc = "LSP References";
+      }
+      {
+        mode = "n";
+        key = "gd";
+        action = "<cmd> lua vim.lsp.buf.implementation() <cr>";
+        options.desc = "LSP Definition";
+      }
+      # Buffers
+      {
+        mode = "n";
+        key = "<leader>b";
+        action = "<cmd> enew <cr>";
+        options.desc = "New Buffer";
+      }
+      {
+        key = "<leader>x";
+        action = "<cmd> bd <cr>";
+        options.desc = "Close Buffer";
+      }
+      # Bufferline
+      {
+        mode = "n";
+        key = "<Tab>";
+        action = "<cmd> BufferLineCycleNext <cr>";
+        options.desc = "Next Buffer";
+      }
+      {
+        mode = "n";
+        key = "<s-Tab>";
+        action = "<cmd> BufferLineCyclePrev <cr>";
+        options.desc = "Previous Buffer";
+      }
+      # Telescope
+      {
+        mode = "n";
         key = "<leader>fa";
-        action = "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>";
+        action = "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <cr>";
         options.desc = "Find All";
       }
       {
         mode = "n";
         key = "<leader>fv";
-        action = "<cmd> Telescope dap variables <CR>";
+        action = "<cmd> Telescope dap variables <cr>";
         options.desc = "Find DAP Variables";
+      }
+      # Move
+      {
+        mode = ["n" "v"];
+        key = "<A-j>";
+        action = "<cmd> MoveLine(1) <cr>";
+        options.desc = "Move Down";
+      }
+      {
+        mode = ["n" "v"];
+        key = "<A-k>";
+        action = "<cmd> MoveLine(-1) <cr>";
+        options.desc = "Move Up";
+      }
+      # Flash
+      {
+        mode = ["n" "x" "o"];
+        key = "s";
+        action = ''<cmd> lua require("flash").jump() <cr>'';
+        options.desc = "Flash Jump";
+      }
+      {
+        mode = ["n" "x" "o"];
+        key = "S";
+        action = ''<cmd> lua require("flash").treesitter() <cr>'';
+        options.desc = "Flash Treesitter";
+      }
+      {
+        mode = ["o"];
+        key = "r";
+        action = ''<cmd> lua require("flash").remote() <cr>'';
+        options.desc = "Flash Remote";
+      }
+      {
+        mode = ["x" "o"];
+        key = "R";
+        action = ''<cmd> lua require("flash").treesitter_search({ remote_op={restore=true,motion=true}}) <cr>'';
+        options.desc = "Flash Treesitter Search";
+      }
+      # Undotree
+      {
+        mode = ["n"];
+        key = "<leader>u";
+        action = "<cmd> UndotreeToggle <cr>";
+        options.desc = "Flash Treesitter";
+      }
+      # Gitsigns
+      {
+        mode = ["n"];
+        key = "<leader>gd";
+        action = "<cmd> Gitsigns diffthis <cr>";
+        options.desc = "Git Diff";
+      }
+      # LSP Saga
+      {
+        mode = ["n"];
+        key = "<c-space>";
+        action = "<cmd> Lspsaga hover_doc <cr>";
+        options.desc = "Show Hover Doc";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>ca";
+        action = "<cmd> Lspsaga code_action <cr>";
+        options.desc = "Show Code Actions";
+      }
+      {
+        mode = ["n"];
+        key = "]d";
+        action = "<cmd> Lspsaga diagnostic_jump_next <cr>";
+        options.desc = "Next Diagnostic";
+      }
+      {
+        mode = ["n"];
+        key = "[d";
+        action = "<cmd> Lspsaga diagnostic_jump_prev <cr>";
+        options.desc = "Previous Diagnostic";
+      }
+      # Trouble
+      {
+        mode = ["n"];
+        key = "<leader>tt";
+        action = "<cmd> TroubleToggle <cr>";
+        options.desc = "Toggle Trouble";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>tw";
+        action = "<cmd> TroubleToggle workspace_diagnostics<cr>";
+        options.desc = "Workspace Trouble";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>td";
+        action = "<cmd> TroubleToggle document_diagnoostics<cr>";
+        options.desc = "Document Trouble";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>tq";
+        action = "<cmd> TroubleToggle quickfix<cr>";
+        options.desc = "Toggle Trouble Quickfix";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>tl";
+        action = "<cmd> TroubleToggle loclist<cr>";
+        options.desc = "Toggle Trouble Loclist";
       }
     ];
     plugins = {
+      noice = {
+        enable = true;
+      };
       telescope = {
         enable = true;
         keymaps = {
@@ -68,23 +276,174 @@
             action = "oldfiles";
             desc = "Find Recent";
           };
+          "<leader>gs" = {
+            action = "git_status";
+            desc = "Git Status";
+          };
+          "<leader>gb" = {
+            action = "git_branches";
+            desc = "Git Branches";
+          };
+          "<leader>gc" = {
+            action = "git_commits";
+            desc = "Git Commits";
+          };
         };
-      };
-      which-key = {
-        enable = true;
       };
       vimtex = {
         enable = true;
         viewMethod = "zathura";
       };
-      lsp = {
+      nvim-autopairs.enable = true;
+      nvim-cmp = {
         enable = true;
-        servers = {
-          ltex = {
-            enable = true;
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "path";}
+          {name = "buffer";}
+        ];
+        mapping = {
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<C-Space>" = {
+            action = "cmp.mapping.complete()";
+            modes = ["i" "s"];
+          };
+          "<Tab>" = {
+            action = ''
+                          function(fallback)
+                            if cmp.visible() then
+                              if #cmp.get_entries() == 1 then
+                                cmp.confirm({ select = true })
+                              else
+                                cmp.select_next_item()
+                              end
+                            else
+                              fallback()
+              end
+                          end
+            '';
+            modes = ["i" "s"];
+          };
+          "<S-Tab>" = {
+            action = ''
+              function(fallback)
+                if cmp.visible() then
+                  cmp.select_prev_item()
+                end
+              end
+            '';
+            modes = ["i" "s"];
           };
         };
       };
+      bufferline.enable = true;
+      surround.enable = true;
+      trouble = {
+        enable = true;
+        signs = {
+          error = "";
+          warning = "";
+          hint = "";
+          information = "";
+          other = "";
+        };
+      };
+      undotree.enable = true;
+      commentary.enable = true;
+      lspsaga = {
+        enable = true;
+        lightbulb.enable = false;
+      };
+      floaterm.enable = true;
+      flash.enable = true;
+      which-key.enable = true;
+      gitsigns.enable = true;
+      nvim-lightbulb.enable = true;
+      lualine.enable = true;
+      alpha = {
+        enable = true;
+        layout = [
+          {
+            type = "padding";
+            val = 2;
+          }
+          {
+            opts = {
+              hl = "Type";
+              position = "center";
+            };
+            type = "text";
+            val = [
+              "  ███╗   ██╗██╗██╗  ██╗██╗   ██╗██╗███╗   ███╗  "
+              "  ████╗  ██║██║╚██╗██╔╝██║   ██║██║████╗ ████║  "
+              "  ██╔██╗ ██║██║ ╚███╔╝ ██║   ██║██║██╔████╔██║  "
+              "  ██║╚██╗██║██║ ██╔██╗ ╚██╗ ██╔╝██║██║╚██╔╝██║  "
+              "  ██║ ╚████║██║██╔╝ ██╗ ╚████╔╝ ██║██║ ╚═╝ ██║  "
+              "  ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚═╝     ╚═╝  "
+            ];
+          }
+          {
+            type = "padding";
+            val = 2;
+          }
+        ];
+      };
+      indent-blankline = {
+        enable = true;
+        indent = {
+          char = "▏";
+        };
+      };
+      fidget.enable = true;
+      dap = {
+        enable = true;
+      };
+      luasnip = {
+        enable = true;
+      };
+      none-ls = {
+        enable = true;
+        sources = {
+          code_actions = {
+            gitsigns.enable = true;
+          };
+          diagnostics = {
+            luacheck.enable = true;
+            flake8.enable = true;
+            eslint.enable = true;
+            cppcheck.enable = true;
+          };
+          formatting = {
+            alejandra.enable = true;
+            black.enable = true;
+            eslint.enable = true;
+            isort.enable = true;
+            jq.enable = true;
+            markdownlint.enable = true;
+            prettier.enable = true;
+            stylua.enable = true;
+          };
+        };
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          ltex.enable = true;
+          digestif.enable = true;
+          nil_ls.enable = true;
+          omnisharp.enable = true;
+          clangd.enable = true;
+          lua-ls.enable = true;
+          pyright.enable = true;
+          cssls.enable = true;
+          html.enable = true;
+          java-language-server.enable = true;
+          tsserver.enable = true;
+          eslint.enable = true;
+        };
+      };
+      treesitter.enable = true;
     };
   };
 }
