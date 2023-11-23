6b257b4d3ab21d51b8460f9526193b5a10140014
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-23 05:31:37 +0100

    feat: update laptop home config

--- home/linus-laptop.nix
+++ home/linus-laptop.nix
@@ -4,29 +4,31 @@
   lib,
   config,
   pkgs,
+  nix-colors,
   ...
-}:
-let
-hyprlandConf = import ./hyprland.nix;
-hyprpaperConf = import ./hyprpaper.nix;
-in
-{
+}: {
   imports = [
     ./linus.nix
   ];
 
-  wayland.windowManager.hyprland.extraConfig = hyprlandConf + ''
-monitor=eDP-1,1920x1080@144,0x0,1
+  colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
 
-bind = $mainMod, 1, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers1.png}"
-bind = $mainMod, 2, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers2.png}"
-bind = $mainMod, 3, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers3.png}"
-bind = $mainMod, 4, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers4.png}"
-bind = $mainMod, 5, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers5.png}"
-bind = $mainMod, 6, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers6.png}"
-  '';
+  wayland.windowManager.hyprland.extraConfig =
+    (import ./hyprland.nix {inherit config pkgs;})
+    + ''
+      monitor=eDP-1,1920x1080@144,0x0,1
 
-  home.file.".config/hypr/hyprpaper.conf".text = hyprpaperConf + ''
-wallpaper = eDP-1,${../secrets/wallpapers/flowers1.png}
-  '';
+      bind = $mainMod, 1, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers1.png}"
+      bind = $mainMod, 2, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers2.png}"
+      bind = $mainMod, 3, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers3.png}"
+      bind = $mainMod, 4, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers4.png}"
+      bind = $mainMod, 5, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers5.png}"
+      bind = $mainMod, 6, exec, hyprctl hyprpaper wallpaper "eDP-1,${../secrets/wallpapers/flowers6.png}"
+    '';
+
+  home.file.".config/hypr/hyprpaper.conf".text =
+    (import ./hyprpaper.nix)
+    + ''
+      wallpaper = eDP-1,${../secrets/wallpapers/flowers1.png}
+    '';
 }
