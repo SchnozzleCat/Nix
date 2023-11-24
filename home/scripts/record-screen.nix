69e725a7ea5b99bf93330f8593b03adee485d7a6
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-24 20:02:32 +0100

    feat: add screen record script

---
+++ home/scripts/record-screen.nix
@@ -0,0 +1,8 @@
+{pkgs}: ''
+  if [[ -z "$(pgrep -l wf-recorder)" ]]; then
+    file=~/Videos/$(date +'%Y-%m-%d-%H%M%S_grim_annotated.mp4')
+    ${pkgs.wf-recorder}/bin/wf-recorder -g "$(${pkgs.slurp}/bin/slurp)" -f "$file"; ${pkgs.xdragon}/bin/xdragon -a -x "$file"
+    else
+  pkill -SIGINT wf-recorder
+    fi
+''
