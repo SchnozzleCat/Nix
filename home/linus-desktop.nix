52dae47d19ca391a8c605ddad63d1ec2d3e08a25
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-24 20:02:20 +0100

    feat: add custom color scheme

--- home/linus-desktop.nix
+++ home/linus-desktop.nix
@@ -11,7 +11,28 @@
     ./linus.nix
   ];
 
-  colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
+  colorScheme = {
+    slug = "schnozzlecat";
+    name = "schnozzlecat";
+    colors = {
+      base00 = "#1F1F28";
+      base01 = "#2a273f";
+      base02 = "#393552";
+      base03 = "#6e6a86";
+      base04 = "#908caa";
+      base05 = "#e0def4";
+      base06 = "#e0def4";
+      base07 = "#56526e";
+      base08 = "#eb6f92";
+      base09 = "#f6c177";
+      base0A = "#ea9a97";
+      base0B = "#3e8fb0";
+      base0C = "#9ccfd8";
+      base0D = "#c4a7e7";
+      base0E = "#f6c177";
+      base0F = "#56526e";
+    };
+  };
 
   wayland.windowManager.hyprland.extraConfig =
     (import ./hyprland.nix {inherit config pkgs;})
