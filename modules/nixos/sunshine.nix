b2143baf8ff888d72cc9bd82f4219a8c925f4364
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-29 13:59:44 +0100

    feat: add sunshine modules

---
+++ modules/nixos/sunshine.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.services.sunshine;
+in {
+  options = {
+    services.sunshine = {
+      enable = mkEnableOption (mdDoc "Sunshine");
+    };
+  };
+
+  config = mkIf config.services.sunshine.enable {
+    environment.systemPackages = [
+      pkgs.sunshine
+    ];
+
+    security.wrappers.sunshine = {
+      owner = "root";
+      group = "root";
+      capabilities = "cap_sys_admin+p";
+      source = "${pkgs.sunshine}/bin/sunshine";
+    };
+
+    systemd.user.services.sunshine = {
+      description = "sunshine";
+      wantedBy = ["graphical-session.target"];
+      serviceConfig = {
+        ExecStart = "${config.security.wrapperDir}/sunshine";
+        ExecStartPre = "${lib.getExe pkgs.xorg.xset} dpms force on";
+        Restart = "always";
+      };
+    };
+  };
+}
