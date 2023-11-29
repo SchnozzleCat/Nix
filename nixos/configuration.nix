edb1c8e907e984240007838ad059d538b9b6e29a
SchnozzleCat <linus@schnozzlecat.xyz>
2023-11-29 14:02:18 +0100

    feat: add user to libvirtd group

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -119,7 +119,7 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel" "networkmanager" "audio" "docker" "corectrl"];
+      extraGroups = ["wheel" "networkmanager" "audio" "docker" "corectrl" "libvirtd"];
     };
   };
 
