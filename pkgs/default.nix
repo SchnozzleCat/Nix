6a27b348cca8e672477d73d0fa256ce9e2fbb011
SchnozzleCat <linus@schnozzlecat.xyz>
2023-12-03 16:53:16 +0100

    feat: add godot4-mono package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs: {
-  materia-transparent-gtk-theme = pkgs.callPackage ./materia-transparent.nix {};
+{pkgs, ...}: {
+  godot-4-mono = pkgs.callPackage ./godot4-mono {};
 }
