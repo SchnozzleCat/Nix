845325fc657e3b7e8c463ad0cb982c2de51c3706
SchnozzleCat <linus@schnozzlecat.xyz>
2023-12-20 00:54:07 +0100

    feat: set TLP for laptop

--- nixos/schnozzlecat-laptop.nix
+++ nixos/schnozzlecat-laptop.nix
@@ -37,6 +37,26 @@
 
   services.xserver.videoDrivers = ["nvidia"];
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+
+      # The following prevents the battery from charging fully to
+      # preserve lifetime. Run `tlp fullcharge` to temporarily force
+      # full charge.
+      # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 50;
+
+      # 100 being the maximum, limit the speed of my CPU to reduce
+      # heat and increase battery usage:
+      CPU_MAX_PERF_ON_AC = 90;
+      CPU_MAX_PERF_ON_BAT = 40;
+    };
+  };
+
   security.pam.yubico = {
     enable = true;
     debug = false;
