eea3dda74e379e28de790f6f8d256bb084bd73f1
SchnozzleCat <linus@schnozzlecat.xyz>
2024-02-03 18:03:44 +0100

    feat: remove splash

--- home/hyprpaper.nix
+++ home/hyprpaper.nix
@@ -1,8 +1,9 @@
 ''
-preload = ${../secrets/wallpapers/flowers1.png}
-preload = ${../secrets/wallpapers/flowers2.png}
-preload = ${../secrets/wallpapers/flowers3.png}
-preload = ${../secrets/wallpapers/flowers4.png}
-preload = ${../secrets/wallpapers/flowers5.png}
-preload = ${../secrets/wallpapers/flowers6.png}
+  preload = ${../secrets/wallpapers/flowers1.png}
+  preload = ${../secrets/wallpapers/flowers2.png}
+  preload = ${../secrets/wallpapers/flowers3.png}
+  preload = ${../secrets/wallpapers/flowers4.png}
+  preload = ${../secrets/wallpapers/flowers5.png}
+  preload = ${../secrets/wallpapers/flowers6.png}
+  splash = false
 ''
