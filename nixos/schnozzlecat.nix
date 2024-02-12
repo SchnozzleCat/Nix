fb98a1a63fd61fd63803f69752c1f51735e2185e
SchnozzleCat <linus@schnozzlecat.xyz>
2024-02-12 09:31:27 +0100

    fix: obs cam

--- nixos/schnozzlecat.nix
+++ nixos/schnozzlecat.nix
@@ -35,6 +35,9 @@
   boot.initrd.kernelModules = ["amdgpu"];
   boot.kernelModules = ["hid-nintendo" "v4l2loopback" "uinput"];
   boot.extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
   boot.kernelParams = ["intel_iommu=on"];
 
   virtualisation.libvirtd = {
