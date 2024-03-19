e44342d4fcab616cebc47d9496835860e1dbc7b2
SchnozzleCat <linus@schnozzlecat.xyz>
2024-03-19 22:04:38 +0100

    feat: Update roslyn-nvim and copilotchat-nvim packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,24 +4,15 @@
 in {
   godot4-mono = pkgs.callPackage ./godot4-mono {};
   # godot4-mono-schnozzlecat = pkgs.callPackage ./godot4-mono-schnozzlecat {};
+  roslyn-ls = pkgs.callPackage ./roslyn-ls {};
   roslyn-nvim = pkgs.vimUtils.buildVimPlugin {
     pname = "roslyn.nvim";
-    version = "4.9.0-3.23604.10";
+    version = "";
     src = pkgs.fetchFromGitHub {
-      owner = "jmederosalvarado";
+      owner = "SchnozzleCat";
       repo = "roslyn.nvim";
-      rev = "4.9.0-3.23604.10";
-      sha256 = "sha256-LWe20cUtZKWJr9tWZgBP2/oZb9ipJTvKJFL5K+TZCrI=";
-    };
-  };
-  obsidian-nvim = pkgs.vimUtils.buildVimPlugin {
-    pname = "obsidian-nvim";
-    version = "v2.3.1";
-    src = pkgs.fetchFromGitHub {
-      owner = "epwalsh";
-      repo = "obsidian.nvim";
-      rev = "v2.3.1";
-      sha256 = "sha256-g9GFq5FMaCcJ6HbnhRgCmioLvaJ4SK6jSioDi5lXeP4=";
+      rev = "42a5661dc4403e0c414f8d5081fd65d17a6fd1bb";
+      sha256 = "sha256-oG3hCszFhvR/rhP2Iz0Vtf8rim1byY/XsDdGYfNRX/w=";
     };
   };
   copilotchat-nvim = pkgs.vimUtils.buildVimPlugin {
@@ -30,8 +21,8 @@ in {
     src = pkgs.fetchFromGitHub {
       owner = "jellydn";
       repo = "CopilotChat.nvim";
-      rev = "90fe665555e529e3bf653ab59cb5cd72ba6174a0";
-      sha256 = "sha256-n+iK97uSOYdlQZSZV3JJHBWKHi7UPPKpKUZm4eUDIGs=";
+      rev = "4b2e631dfd7e08507dd083a18480fe71a7bf8717";
+      sha256 = "sha256-ft42fmJ4sJqo8P60JO41zTyTarNGL2anpNXrHpDFbbk=";
     };
   };
 }
