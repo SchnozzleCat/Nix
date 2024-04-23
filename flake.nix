582f56f1e0fc8ecda64bc83338a1cdb4d73b2253
SchnozzleCat <linus@schnozzlecat.xyz>
2024-04-23 02:14:17 +0200

    feat: use hyprland git

--- flake.nix
+++ flake.nix
@@ -13,6 +13,8 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    hyprland.url = "github:hyprwm/Hyprland";
+
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
