809040673185fdba4ce3cdb75fa1fe34fae25fbc
SchnozzleCat <linus@schnozzlecat.xyz>
2024-05-16 03:51:57 +0200

    feat: adapt from template

--- nixos/schnozzlecat-server.nix
+++ nixos/schnozzlecat-server.nix
@@ -11,17 +11,26 @@
     ./hardware-configuration-${hostname}.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  hardware = {
-    raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-    deviceTree = {
-      enable = true;
-      filter = "*rpi-4-*.dtb";
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = ["xhci_pci" "usbhid" "usb_storage"];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
     };
   };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = ["noatime"];
+    };
+  };
+
+  services.openssh.enable = true;
+
+  hardware.enableRedistributableFirmware = true;
 
   systemd.services.create-modules-alias-symlink = {
     description = "Create symlink for kernel modules.alias";
