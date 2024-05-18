1a8581c46175c613961c627d37b905e2cbaf6662
SchnozzleCat <linus@schnozzlecat.xyz>
2024-05-18 02:58:27 +0200

    feat: add blueman; use cpufreq

--- nixos/schnozzlecat-laptop.nix
+++ nixos/schnozzlecat-laptop.nix
@@ -54,6 +54,7 @@
       ];
     };
   };
+  services.blueman.enable = true;
 
   sound.mediaKeys.enable = true;
   services.actkbd = {
@@ -74,23 +75,16 @@
 
   services.xserver.videoDrivers = ["nvidia"];
 
-  services.tlp = {
+  services.thermald.enable = true;
+  services.auto-cpufreq.settings = {
     enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-
-      # The following prevents the battery from charging fully to
-      # preserve lifetime. Run `tlp fullcharge` to temporarily force
-      # full charge.
-      # https://linrunner.de/tlp/faq/battery.html#how-to-choose-good-battery-charge-thresholds
-      START_CHARGE_THRESH_BAT0 = 40;
-      STOP_CHARGE_THRESH_BAT0 = 50;
-
-      # 100 being the maximum, limit the speed of my CPU to reduce
-      # heat and increase battery usage:
-      CPU_MAX_PERF_ON_AC = 90;
-      CPU_MAX_PERF_ON_BAT = 40;
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
     };
   };
 
