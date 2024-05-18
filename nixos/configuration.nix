8bbea65861c9d4db11b42697138d4472a1984c90
SchnozzleCat <linus@schnozzlecat.xyz>
2024-05-18 02:44:02 +0200

    feat: add wireguard

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,6 +23,43 @@
     "fs.file-max" = 524288;
   };
 
+  # Enable WireGuard
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      ips = ["10.100.0.2/24"];
+      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "home/linus/.nixos/secrets/wireguard/private.key";
+
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
+
+        {
+          # Public key of the server (not a file path).
+          publicKey = "+t7XYAuU5TXUgzffTdATF14bP/Yi92exQrIqF2ZjmC8=";
+
+          # Forward all the traffic via VPN.
+          allowedIPs = ["0.0.0.0/0"];
+          # Or forward only particular subnets
+          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+
+          # Set this to the server IP and port.
+          endpoint = "roamer.servebeer.com:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+
   systemd.services.create-modules-alias-symlink = {
     description = "Create symlink for kernel modules.alias";
     after = ["systemd-tmpfiles-setup.service"];
@@ -43,8 +80,7 @@
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [47984 47989 47990 48010];
-    allowedUDPPorts = [47998 47999 48000 48002];
+    allowedUDPPorts = [51820]; # WireGuard
   };
 
   # Yubikey
--- nixos/schnozzlecat-server.nix
+++ nixos/schnozzlecat-server.nix
@@ -4,7 +4,8 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+in {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration-schnozzlecat-server.nix
@@ -16,6 +17,83 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
+    };
+  };
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+
+    containers = {
+      pihole = {
+        autoStart = true;
+        image = "pihole/pihole:latest";
+        volumes = [
+          "/var/lib/pihole:/etc/pihole"
+          "/var/lib/dnsmasq.d:/etc/dnsmasq.d"
+        ];
+        ports = [
+          "53:53/tcp"
+          "53:53/udp"
+          "80:80/tcp"
+        ];
+        environment = {
+          ServerIP = "localhost";
+        };
+        extraOptions = [
+          "--cap-add=NET_ADMIN"
+          "--dns=127.0.0.1"
+          "--dns=1.1.1.1"
+        ];
+        workdir = "/var/lib/pihole/";
+      };
+    };
+  };
+
+  # enable NAT
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "eth0";
+  networking.nat.internalInterfaces = ["wg0"];
+  networking.firewall = {
+    allowedUDPPorts = [51820];
+  };
+
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = ["10.100.0.1/24"];
+
+      # The port that WireGuard listens to. Must be accessible by the client.
+      listenPort = 51820;
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # This undoes the above command
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/home/linus/wireguard-keys/private.key";
+
+      peers = [
+        # List of allowed peers.
+        {
+          # Feel free to give a meaning full name
+          # Public key of the peer (not a file path).
+          publicKey = "Jq90boTS2KFk0NDDSTPuQV6wKUF9PjRMQPzbsHdfe0U=";
+          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+          allowedIPs = ["10.100.0.2/32"];
+        }
+      ];
     };
   };
 
@@ -51,7 +129,7 @@
   users.users = {
     linus = {
       isNormalUser = true;
-      extraGroups = ["wheel"];
+      extraGroups = ["wheel" "docker"];
       initialPassword = "password";
       createHome = true;
       home = "/home/linus";
