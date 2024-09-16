62b0f61aefcc698ceed5ea0a8258782a65472850
SchnozzleCat <linus@schnozzlecat.xyz>
2024-09-16 20:11:24 +0200

    feat: switch avante provider to copilot

--- home/neovim.nix
+++ home/neovim.nix
@@ -493,33 +493,7 @@
         behaviour = {
           auto_suggestions = false,
         },
-        provider = "ollama",
-        vendors = {
-          ---@type AvanteProvider
-          ollama = {
-            ["local"] = true,
-            endpoint = "127.0.0.1:11434/v1",
-            model = "deepseek-coder-v2:latest",
-            parse_curl_args = function(opts, code_opts)
-              return {
-                url = opts.endpoint .. "/chat/completions",
-                headers = {
-                  ["Accept"] = "application/json",
-                  ["Content-Type"] = "application/json",
-                },
-                body = {
-                  model = opts.model,
-                  messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-                  max_tokens = 2048,
-                  stream = true,
-                },
-              }
-            end,
-            parse_response_data = function(data_stream, event_state, opts)
-              require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-            end,
-          },
-        },
+        provider = "copilot",
       })
       require("custom")
     '';
