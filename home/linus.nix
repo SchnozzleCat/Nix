1c19f3146b8b242c5e70d0a674335f1264773153
SchnozzleCat <linus@schnozzlecat.xyz>
2024-09-16 20:11:09 +0200

    feat: reduce effect strength of night light filter

--- home/linus.nix
+++ home/linus.nix
@@ -382,7 +382,7 @@ in {
     varying vec2 v_texcoord;
     uniform sampler2D tex;
 
-    const float temperature = 3800.0;
+    const float temperature = 4300.0;
     const float temperatureStrength = 1.0;
 
     #define WithQuickAndDirtyLuminancePreservation
