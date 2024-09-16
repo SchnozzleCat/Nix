a4394b6e587fcf4f9b6d55edd4fc7f1b27577c54
SchnozzleCat <linus@schnozzlecat.xyz>
2024-09-16 20:12:14 +0200

    feat: remove unused packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,6 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 {pkgs, ...}: {
-  godot4-mono = pkgs.callPackage ./godot4-mono {};
-  netcoredbg = pkgs.callPackage ./netcoredbg {};
   avante-nvim = (pkgs.callPackage ./avante-nvim {}).overrideAttrs {
     dependencies = with pkgs.vimPlugins; [
       dressing-nvim
@@ -10,6 +8,4 @@
       plenary-nvim
     ];
   };
-
-  # godot4-mono-schnozzlecat = pkgs.callPackage ./godot4-mono-schnozzlecat {};
 }
