f2532162e6a4d0337af6555fac2b67720a17a7ae
SchnozzleCat <linus@schnozzlecat.xyz>
2024-11-23 19:55:38 +0100

    feat: remove unused packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,4 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 {pkgs, ...}: {
-  avante-nvim = (pkgs.callPackage ./avante-nvim {}).overrideAttrs {
-    dependencies = with pkgs.vimPlugins; [
-      dressing-nvim
-      nui-nvim
-      plenary-nvim
-    ];
-  };
 }
