659faa225b92f1c852753e59b2991c42da202ab9
SchnozzleCat <linus@schnozzlecat.xyz>
2024-11-23 20:45:40 +0100

    feat: update flake; pin hyprland nixpkgs

--- flake.nix
+++ flake.nix
@@ -18,7 +18,10 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
--- home/hyprland.nix
+++ home/hyprland.nix
@@ -129,10 +129,6 @@ in ''
       }
 
       rounding = 5
-      drop_shadow = yes
-      shadow_range = 4
-      shadow_render_power = 3
-      col.shadow = rgba(1a1a1aee)
   }
 
   # layerrule = blur, waybar
