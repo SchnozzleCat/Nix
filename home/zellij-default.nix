ca897f774c002a0981ac085a36b9a65c13145cd6
SchnozzleCat <linus@schnozzlecat.xyz>
2024-12-26 11:36:58 +0100

    feat: change zjstatus look

--- home/zellij-default.nix
+++ home/zellij-default.nix
@@ -16,10 +16,10 @@
               border_char     " "
               border_position "top"
 
-              format_center  "{tabs}"
+              format_left  "{tabs}"
 
-              tab_normal               "#[fg=#A6ADC8,bg=#1E1E2E]   {name} {fullscreen_indicator}{sync_indicator}{floating_indicator}  "
-              tab_active               "#[fg=#B4BEFE,bg=#45475A,bold]   {name} {fullscreen_indicator}{sync_indicator}{floating_indicator}  "
+              tab_normal               "#[fg=#A6ADC8] {name} {fullscreen_indicator}{sync_indicator}{floating_indicator}"
+              tab_active               "#[fg=#B4BEFE,bold] {name} {fullscreen_indicator}{sync_indicator}{floating_indicator}"
               tab_fullscreen_indicator ""
               tab_sync_indicator       ""
               tab_floating_indicator   "󰉈"
