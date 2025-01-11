4a148151f911a3053e7b564bc1937e4b5bf5615e
SchnozzleCat <linus@schnozzlecat.xyz>
2025-01-11 22:27:34 +0100

    feat: remove nvidia drivers; disable yubico pam; remove unused sound attribute

--- nixos/schnozzlecat-laptop.nix
+++ nixos/schnozzlecat-laptop.nix
@@ -36,7 +36,6 @@
   };
   services.blueman.enable = true;
 
-  sound.mediaKeys.enable = true;
   services.actkbd = {
     enable = true;
     bindings = [
@@ -53,8 +52,6 @@
     ];
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
-
   services.thermald.enable = true;
   services.auto-cpufreq.settings = {
     enable = true;
@@ -66,23 +63,6 @@
       governor = "performance";
       turbo = "auto";
     };
-  };
-
-  security.pam.yubico = {
-    enable = true;
-    debug = false;
-    mode = "challenge-response";
-    id = ["23767516"];
-    control = "required";
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   hardware.cpu.intel.updateMicrocode = true;
