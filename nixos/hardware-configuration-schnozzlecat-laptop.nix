7af0743bcc99519a2fcacdc1f4ad8a4bad4970bb
SchnozzleCat <linus@schnozzlecat.xyz>
2025-01-11 22:25:32 +0100

    feat: update laptop configuration file

--- nixos/hardware-configuration-schnozzlecat-laptop.nix
+++ nixos/hardware-configuration-schnozzlecat-laptop.nix
@@ -8,31 +8,32 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c6f05f89-b24f-4535-a347-a00c93c5f70a";
+    { device = "/dev/disk/by-uuid/04f7ee4b-132d-43e4-8c2a-dcc9279deffd";
       fsType = "ext4";
     };
 
+  boot.initrd.luks.devices."nixos".device = "/dev/disk/by-uuid/112d3620-b0a3-498c-bfc7-3e446a94c5a7";
+
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/28CE-6228";
+    { device = "/dev/disk/by-uuid/C205-6175";
       fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/ebdfc323-6904-4391-b153-0367b0192be7"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
