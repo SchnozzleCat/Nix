0db5ef894d9dde7be151a92d667dc8e6d466f13b
SchnozzleCat <linus@schnozzlecat.xyz>
2025-01-13 15:47:17 +0100

    feat: disable portal package until shutdown hang is fixed

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -146,7 +146,6 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
