15c460187f63a43ecd9dd60315bb9fc480b979f9
SchnozzleCat <linus@schnozzlecat.xyz>
2025-03-26 16:42:55 +0100

    feat: remove emoji and ripgrep providers from blink

--- home/neovim/plugins.nix
+++ home/neovim/plugins.nix
@@ -86,37 +86,7 @@
             "path"
             "snippets"
             "buffer"
-            "emoji"
           ];
-          providers = {
-            ripgrep = {
-              async = true;
-              module = "blink-ripgrep";
-              name = "Ripgrep";
-              score_offset = -1000;
-              opts = {
-                prefix_min_len = 3;
-                context_size = 5;
-                max_filesize = "1M";
-                project_root_marker = ".git";
-                project_root_fallback = true;
-                search_casing = "--ignore-case";
-                additional_rg_options = {};
-                fallback_to_regex_highlighting = true;
-                ignore_paths = {};
-                additional_paths = {};
-                debug = false;
-              };
-            };
-            emoji = {
-              module = "blink-emoji";
-              name = "Emoji";
-              score_offset = 15;
-              opts = {
-                insert = true;
-              };
-            };
-          };
         };
         signature = {
           enabled = true;
