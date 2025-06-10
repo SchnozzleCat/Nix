87077d8ca212357d3619d68453834beadd2dec46
SchnozzleCat <linus@schnozzlecat.xyz>
2025-06-10 11:40:35 +0200

    feat: use mostly default roslyn-nvim settings

--- home/neovim/plugins.nix
+++ home/neovim/plugins.nix
@@ -974,57 +974,11 @@
         }
         require('rzls').setup {}
         require("roslyn").setup({
-        args = {
-            '--stdio',
-            '--logLevel=Information',
-            '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-            '--razorSourceGenerator=' .. vim.fs.joinpath(
-              vim.fn.stdpath 'data' --[[@as string]],
-              'mason',
-              'packages',
-              'roslyn',
-              'libexec',
-              'Microsoft.CodeAnalysis.Razor.Compiler.dll'
-            ),
-            '--razorDesignTimePath=' .. vim.fs.joinpath(
-              vim.fn.stdpath 'data' --[[@as string]],
-              'mason',
-              'packages',
-              'rzls',
-              'libexec',
-              'Targets',
-              'Microsoft.NET.Sdk.Razor.DesignTime.targets'
-            ),
-          },
-          config = {
-            handlers = require 'rzls.roslyn_handlers',
-            on_attach = _M.lspOnAttach,
-            capabilities = __lspCapabilities(),
-            filetypes = {"cs"},
-            filewatching = true,
-            settings = {
-              ["csharp|inlay_hints"] = {
-                  csharp_enable_inlay_hints_for_implicit_object_creation = true,
-                  csharp_enable_inlay_hints_for_implicit_variable_types = true,
-                  csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-                  csharp_enable_inlay_hints_for_types = true,
-                  dotnet_enable_inlay_hints_for_indexer_parameters = true,
-                  dotnet_enable_inlay_hints_for_literal_parameters = true,
-                  dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-                  dotnet_enable_inlay_hints_for_other_parameters = true,
-                  dotnet_enable_inlay_hints_for_parameters = true,
-                  dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-                  dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-                  dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-              },
-              ["csharp|background_analysis"] = {
-                dotnet_compiler_diagnostics_scope = "fullSolution"
-              },
-              ["csharp|code_lens"] = {
-                dotnet_enable_references_code_lens = true,
-              },
+          settings = {
+            ["csharp|projects"] = {
+                dotnet_enable_file_based_programs = true,
             },
-          }
+        },
         })
       '';
       onAttach = ''
