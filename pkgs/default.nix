5f2349f401f98aebdab423a831eaaf2cbe9b35eb
SchnozzleCat <linus@schnozzlecat.xyz>
2025-06-24 00:17:37 +0200

    feat: remove custom goose package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,6 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-{pkgs, ...}: {
-  inky = pkgs.callPackage ./inky {};
-  goose-cli = pkgs.callPackage ./goose-cli {};
+{ pkgs, ... }:
+{
+  inky = pkgs.callPackage ./inky { };
 }
