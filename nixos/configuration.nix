495d6f9620741d855f8b128f4b15356554403d14
SchnozzleCat <linus@schnozzlecat.xyz>
2025-11-15 11:25:38 +0100

    feat: add user to trusted users

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -392,6 +392,7 @@
       nix-path = config.nix.nixPath;
       substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      trusted-users = ["linus"];
     };
     # Opinionated: disable channels
     channel.enable = false;
