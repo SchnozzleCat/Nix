95c0ed6671b0b9de738f0c44b774bb68f4e3f02e
SchnozzleCat <linus@schnozzlecat.xyz>
2026-05-25 12:51:20 +0200

    feat: update flake

--- home/config/hyprland.nix
+++ home/config/hyprland.nix
@@ -128,8 +128,6 @@ in ''
   }
 
   dwindle {
-      # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
-      pseudotile = yes # master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
       preserve_split = yes # you probably want this
       #permanent_direction_override = true
   }
