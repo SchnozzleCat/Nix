ab757cebe0f7d9627c9239defcc21f9c42f1c861
SchnozzleCat <linus@schnozzlecat.xyz>
2026-07-20 00:43:37 +0200

    feat: use transparent neovim theme

--- home/neovim/plugins.nix
+++ home/neovim/plugins.nix
@@ -54,7 +54,6 @@ in {
     fugitive.enable = true;
     transparent = {
       enable = true;
-      lazyLoad.settings.event = "DeferredUIEnter";
     };
     dressing.enable = true;
     notify = {
@@ -948,9 +947,112 @@ in {
 
     lualine = {
       enable = true;
-      lazyLoad.settings.event = "BufEnter";
       settings = {
-        theme = "teide";
+        theme = let
+          colors = {
+            black = "#282828";
+            white = "#ebdbb2";
+            red = "#fb4934";
+            green = "#b8bb26";
+            blue = "#83a598";
+            yellow = "#fe8019";
+            gray = "#a89984";
+            darkgray = "#3c3836";
+            lightgray = "#504945";
+            inactivegray = "#7c6f64";
+          };
+        in {
+          normal = {
+            a = {
+              bg = colors.gray;
+              fg = colors.black;
+              gui = "bold";
+            };
+            b = {
+              bg = colors.lightgray;
+              fg = colors.white;
+            };
+            c = {
+              bg = colors.darkgray;
+              fg = colors.gray;
+            };
+          };
+          insert = {
+            a = {
+              bg = colors.blue;
+              fg = colors.black;
+              gui = "bold";
+            };
+            b = {
+              bg = colors.lightgray;
+              fg = colors.white;
+            };
+            c = {
+              bg = colors.lightgray;
+              fg = colors.white;
+            };
+          };
+          visual = {
+            a = {
+              bg = colors.yellow;
+              fg = colors.black;
+              gui = "bold";
+            };
+            b = {
+              bg = colors.lightgray;
+              fg = colors.white;
+            };
+            c = {
+              bg = colors.inactivegray;
+              fg = colors.black;
+            };
+          };
+          replace = {
+            a = {
+              bg = colors.red;
+              fg = colors.black;
+              gui = "bold";
+            };
+            b = {
+              bg = colors.lightgray;
+              fg = colors.white;
+            };
+            c = {
+              bg = colors.black;
+              fg = colors.white;
+            };
+          };
+          command = {
+            a = {
+              bg = colors.green;
+              fg = colors.black;
+              gui = "bold";
+            };
+            b = {
+              bg = colors.lightgray;
+              fg = colors.white;
+            };
+            c = {
+              bg = colors.inactivegray;
+              fg = colors.black;
+            };
+          };
+          inactive = {
+            a = {
+              bg = colors.darkgray;
+              fg = colors.gray;
+              gui = "bold";
+            };
+            b = {
+              bg = colors.darkgray;
+              fg = colors.gray;
+            };
+            c = {
+              bg = colors.darkgray;
+              fg = colors.gray;
+            };
+          };
+        };
         winbar = {
           lualine_a = [""];
           lualine_b = [""];
