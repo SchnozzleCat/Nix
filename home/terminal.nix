ba0ac344d791a6386fc606437434b37c94cca07a
SchnozzleCat <linus@schnozzlecat.xyz>
2026-07-20 00:39:11 +0200

    featT: disable themes for btop and starship

--- home/terminal.nix
+++ home/terminal.nix
@@ -208,13 +208,6 @@ in {
 
   programs.btop = {
     enable = true;
-    settings = {
-      color_theme = "teide-dark";
-      theme_background = false;
-      truecolor = true;
-      rounded_corners = true;
-      vim_keys = true;
-    };
     themes = {
       teide-dark = ''
         # Theme: teide_dark
@@ -297,68 +290,6 @@ in {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
-    settings = {
-      line_break = {
-        disabled = true;
-      };
-      custom.jj = {
-        description = "The current jj status";
-        when = "jj --ignore-working-copy root";
-        symbol = "🥋 ";
-        command = ''
-          jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
-            separate(" ",
-              change_id.shortest(4),
-              bookmarks,
-              "|",
-              concat(
-                if(conflict, "💥"),
-                if(divergent, "🚧"),
-                if(hidden, "👻"),
-                if(immutable, "🔒"),
-              ),
-              raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
-              raw_escape_sequence("\x1b[1;32m") ++ coalesce(
-                truncate_end(29, description.first_line(), "…"),
-                "(no description set)",
-              ) ++ raw_escape_sequence("\x1b[0m"),
-            )
-          '
-        '';
-      };
-
-      git_status.disabled = true;
-      custom.git_status = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_status";
-        style = "";
-        description = "Only show git_status if we're not in a jj repo";
-      };
-
-      git_commit.disabled = true;
-      custom.git_commit = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_commit";
-        style = "";
-        description = "Only show git_commit if we're not in a jj repo";
-      };
-
-      git_metrics.disabled = true;
-      custom.git_metrics = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_metrics";
-        description = "Only show git_metrics if we're not in a jj repo";
-        style = "";
-      };
-
-      git_branch.disabled = true;
-      custom.git_branch = {
-        when = "! jj --ignore-working-copy root";
-        command = "starship module git_branch";
-        description = "Only show git_branch if we're not in a jj repo";
-        style = "";
-      };
-    };
   };
 
   programs.zellij = {
