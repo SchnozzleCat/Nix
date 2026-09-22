01e0b33e88ba5cda6adf3d7bb2507ccaa9dd7ce8
SchnozzleCat <linus@schnozzlecat.xyz>
2026-09-22 15:12:45 +0200

    feat: update flake

--- hosts/rpi5/server.nix
+++ hosts/rpi5/server.nix
@@ -227,7 +227,10 @@
   # Making legacy nix commands consistent as well, awesome!
 
   nix.settings = {
-    experimental-features = "nix-command flakes";
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
     auto-optimise-store = true;
     trusted-users = ["linus"];
   };
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -443,7 +443,10 @@ R1tH9/wg2FSXDNZKCTJ5iSfZLBrL
 
     settings = {
       # Enable flakes and new 'nix' command
-      experimental-features = "nix-command flakes";
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       # Opinionated: disable global registry
       flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
